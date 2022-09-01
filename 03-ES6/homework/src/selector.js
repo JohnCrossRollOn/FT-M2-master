5f24c49bab4022b65e56217b23179a84daf13c7f
JohnCrossRollOn <juanrolon54@outlook.com>
2022-08-31 23:50:20 -0300

    DOM and CS6 are great!

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -1,24 +1,21 @@
-var traverseDomAndCollectElements = function(matchFunc, startEl) {
+var traverseDomAndCollectElements = function(matchFunc, startEl = document.body) {
   var resultSet = [];
-
-  if (typeof startEl === "undefined") {
-    startEl = document.body;
-  }
 
   // recorre el árbol del DOM y recolecta elementos que matchien en resultSet
   // usa matchFunc para identificar elementos que matchien
-
   // TU CÓDIGO AQUÍ
-  
-};
-
+const allElements = document.getElementsByTagName("*");
+for (let elements of allElements) {
+  if (matchFunc(elements)){resultSet = [...resultSet, elements]}
+}
+  return resultSet
+}
 // Detecta y devuelve el tipo de selector
 // devuelve uno de estos tipos: id, class, tag.class, tag
 
 
 var selectorTypeMatcher = function(selector) {
-  // tu código aquí
-  
+  return [...selector][0]==='#'?'id':[...selector][0]==='.'?'class':[...selector].includes('.', 1)?'tag.class':'tag'
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -30,14 +27,25 @@ var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") { 
-   
+    matchFunction = (element)=>{
+      return ('#'+ element.id) === selector
+    };
   } else if (selectorType === "class") {
-    
-  } else if (selectorType === "tag.class") {
-    
+    matchFunction = (element)=>{
+      let classes = element.className.split(/(\s+)/).filter( e => e.trim().length > 0)
+      return classes.map(v=>'.'+v).includes(selector)
+    };
   } else if (selectorType === "tag") {
-    
-  }
+    matchFunction = (element)=>{
+      return (element.localName) === selector
+    };
+  } else if (selectorType === "tag.class") {
+    matchFunction = (element)=>{//element = img.thumbnail
+      let selectors = selector.split(/,|\./)
+      let classes = element.className.split(/(\s+)/).filter( e => e.trim().length > 0);
+      return (element.localName === selectors[0]) && (classes.includes((selectors[1])))
+    };
+  } 
   return matchFunction;
 };
 
