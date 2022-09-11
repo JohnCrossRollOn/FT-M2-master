9a53603e4917cdf169c96bb38eb99968d0f69d85
JohnCrossRollOn <juanrolon54@outlook.com>
2022-09-10 21:44:38 -0300

    Added "error" indicator

--- 06-React-Intro/homework/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Style from './SearchBar.module.css';
 import Error from './error.jsx';
-import App from '../App';
 export default function SearchBar(props) {
   const [searchInput, setSearchInput] = useState("");
 
@@ -21,7 +20,7 @@ export default function SearchBar(props) {
     <div className={Style.buscador}>
       <input autoFocus onKeyDown={enter} value={searchInput} onChange={(event)=>{props.setSearchFailed(false);setSearchInput(event.target.value)}} type="text" name="search" id="search" placeholder="Search your city" className={Style.input}/>
       <button onClick={()=>{Search()}} className={Style.button}>Search</button>
-      {props.searchStatus?<Error value={'incorrect cityName'}></Error>:null}
+      {props.searchStatus?<Error value={'error'}></Error>:null}
     </div>
       <span className={Style.texto}>&lt;Weather React App Homework/&gt;</span> 
     </div>
